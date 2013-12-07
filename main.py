554cb5b2470bd69f7972d5613eee72595f9f6d54
Stuart A. Doherty <stu.doherty@gmail.com>
2013-12-06 23:30:09 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import webapp2
+from google.appengine.ext import ndb
+
+class Game(ndb.Model):
+    # Date
+    date = ndb.DateTimeProperty(auto_now=True)
+    # Location
+    location = ndb.GeoPtProperty()
+    # Team #1 colour, score
+    # Team #2 colour, score
+    
+    # MAYBE
+    # Unique Observers
+    # Unique Contributors
+    pass
+
+class CreateGame(webapp2.RequestHandler):
+    def put(self):
+        
+        pass
+
+class ScoreboardApp(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello, webapp2! STU!!')
+
+routes = [
+    (r'/', 'main.ScoreboardApp'),
+    (r'/newgame', 'main.CreateGame'), # TODO Request create game "PUT"
+    
+    # TODO Request game list "GET"
+    # TODO Request game update "GET"
+
+     
+]
+
+app = webapp2.WSGIApplication(routes=routes, debug=True)
