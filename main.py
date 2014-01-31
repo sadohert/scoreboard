d9320c7b7db0c40ffa0760423c6bfc48202c97d4
Stuart A. Doherty <stu.doherty@gmail.com>
2014-01-31 06:42:09 -0500

    First substantial push into building NewGame handler and associated testing.
    Refining data models, working out flow.  Still very rough work, but
    wanted to get it backed up

--- main.py
+++ main.py
@@ -1,29 +1,142 @@
+import logging
+import datetime
+import json
+
 import webapp2
 from google.appengine.ext import ndb
+from google.appengine.api import search
+from google.appengine.ext import db
 
-class Game(ndb.Model):
+class TeamEntity(ndb.Model):
+    name = ndb.StringProperty()
+    colour = ndb.StringProperty()
+    score = ndb.IntegerProperty(default=0)
+    
+    @classmethod
+    def fromTuple(cls, teamTuple):
+        return TeamEntity(name=teamTuple[0], colour=teamTuple[1])
+        
+        
+
+class GameEntity(ndb.Model):
     # Date
     date = ndb.DateTimeProperty(auto_now_add=True)
     # Location
     location = ndb.GeoPtProperty()
     # Team #1 colour, score
     # Team #2 colour, score
-    
-    
+    teams = ndb.StructuredProperty(TeamEntity, repeated=True)
+    # Game start time
+    start_time = ndb.TimeProperty()
+    description = ndb.StringProperty()
+    type = ndb.StringProperty()
+    age_category = ndb.IntegerProperty()
+    gender_category = ndb.IntegerProperty()
     # MAYBE
     # Unique Observers
     # Unique Contributors
-    pass
+    def toJson(self):
+        pass
+
+# class Game(object):
+#     ''' Wrapper class for GameEntity that will incorporate the Search API
+#     functionality
+#     '''
+#     def __init__(self, newgame_json):
+#         # Data checking
+#         # Do I want to do any verification of "start_time"?
+#         
+#         ng = GameEntity(location=ndb.GeoPt("%s, %s" % (newgame_json['location']['lat'], newgame_json['location']['lon'])), 
+#                         teams=[TeamEntity.fromTuple(newgame_json['teams'][0]),
+#                                TeamEntity.fromTuple(newgame_json['teams'][1])],
+#                         start_time=datetime.time(newgame_json['start_time']),
+#                         description=newgame_json['description'])
+#         ng.put()
+#         self.game_entity = ng
+
+
+class GameDocument(object):
+    '''Encapsulates the document fields of a Game'''
+    _INDEX_NAME = 'GAMES'
+    #getGeoPt
+    #setGeoPt
+    #getOpponents
+    #setOpponents
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def getIndex(cls):
+        return search.Index(name=cls._INDEX_NAME)
+
+    @classmethod
+    def getDoc(cls, doc_id):
+        """Return the document with the given doc id. One way to do this is via
+        the get_range method, as shown here.  If the doc id is not in the
+        index, the first doc in the index will be returned instead, so we need
+        to check for that case."""
+        if not doc_id:
+            return None
+        try:
+            index = cls.getIndex()
+            response = index.get_range(
+                  start_id=doc_id, limit=1, include_start_object=True)
+            if response.results and response.results[0].doc_id == doc_id:
+                return response.results[0]
+            return None
+        except search.InvalidRequest: # catches ill-formed doc ids
+            return None
+
+    @classmethod
+    def removeDocById(cls, doc_id):
+        """Remove the doc with the given doc id."""
+        try:
+            cls.getIndex().delete(doc_id)
+        except search.Error:
+            logging.exception("Error removing doc id %s.", doc_id)
+
+    @classmethod
+    def add(cls, documents):
+        """wrapper for search index add method; specifies the index name."""
+        try:
+            return cls.getIndex().put(documents)
+        except search.Error:
+            logging.exception("Error adding documents.")
+    
 
 class GetGames(webapp2.RequestHandler):
     def get(self):
         self.response.write('<implement games list>')
+        # Output to json
         pass
 
 class CreateGame(webapp2.RequestHandler):
-    def put(self):
-        # tEST COMMENT
-        pass
+    def post(self):
+        '''Handles the post request accepting a json string formatted by the client
+
+        '''
+        #logging.info('')
+        ng_string = self.request.POST['newgame']
+        if ng_string:
+            newgame_json = json.loads(ng_string)
+            # Check 'ng' app version, See #19
+            
+            # Create Game Model
+            ng = GameEntity(location=ndb.GeoPt("%s, %s" % (newgame_json['location']['lat'], newgame_json['location']['lon'])), 
+                            teams=[TeamEntity.fromTuple(newgame_json['teams'][0]),
+                                   TeamEntity.fromTuple(newgame_json['teams'][1])],
+                            start_time=datetime.time(newgame_json['start_time']),
+                            description=newgame_json['description'],
+                            type=newgame_json['type'])
+            ng.put()
+            # return response with new_game datastore ID.  Possibly json version
+            # of game as well
+            self.response.headers['Content-Type'] = 'application/json'
+            self.response.write(json.dumps(db.to_dict(ng)))
+        else:
+            # Error.  This handler shouldn't be called without a proper
+            # json object to define a new game
+            logging.error("Error in CreateGame: %s", 'No defined json')
 
 class ScoreboardApp(webapp2.RequestHandler):
     def get(self):
--- tests/test_all.py
+++ tests/test_all.py
@@ -5,11 +5,12 @@ Created on Jan 17, 2014
 '''
 import unittest2
 import webapp2
+import json
 
 # from the app main.py
 import main
 
-class TestHandlers(unittest2.TestCase):
+class TestGeneric(unittest2.TestCase):
     def test_root(self):
         # Build a request object passing the URI path to be tested.
         # You can also pass headers, query arguments etc.
@@ -19,3 +20,53 @@ class TestHandlers(unittest2.TestCase):
         # Let's check if the response is correct.
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.body, 'Hello, webapp2! STU!!')
+
+class TestGameCreation(unittest2.TestCase):
+        
+    def test_basic(self):
+        '''Basic first test of handler that doesn't implement 'GET'
+        '''
+        request = webapp2.Request.blank('/newgame')
+        # Get a response for that request.
+        response = request.get_response(main.app)
+        # Let's check if the response is correct.
+        self.assertEqual(response.status_int, 405)
+        
+    def test_newGame(self):
+        ng_json = '''
+            {
+              "version": "0", 
+              "start_time": "19:00",
+              "description": "League play between U13 boys team", 
+              "age_category": 3,
+              "gender_category": 0, 
+              "type": 0, 
+              "location": {
+                "lat": 50, 
+                "lon": 120
+              }, 
+              "teams": [
+                [
+                  "Waterloo", 
+                  "yellow"
+                ], 
+                [
+                  "Kitchener", 
+                  "green"
+                ]
+              ]
+            }
+        '''
+        ng_dict = {}
+        ng_dict['newgame'] = json.dumps(json.loads(ng_json))
+        
+        post_request = webapp2.Request.blank('/newgame', POST=ng_dict)
+
+        # Get a response for that request.
+        response = post_request.get_response(main.app)
+        # Let's check if the response is correct.
+        self.assertEqual(response.status_int, 200)
+        
+        # Verify response.body
+        
+            
