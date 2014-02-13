45124e6eec63a3e775be38ee03e4feafcf0d48f8
Stuart A. Doherty <stu.doherty@gmail.com>
2014-02-13 06:22:35 -0500

    Implemented more of the game meta information (age, gender,description)
    Was about to enhance the ages field on the new game form in the same way
    the genders field is enhanced.

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -9,9 +9,18 @@ angular.module('myApp.controllers', []).
   .controller('MyCtrl2', [function() {
 
   }])
-  .controller('NewGame', [function() {
-
-
+  .controller('NewGame', ['$scope', function($scope) {
+	  $scope.colours = [{name:'red'},{name:'blue'}, {name:'black'}]
+	  
+	  $scope.start_times = ['17:00', '17:30', '18:00', '18:30']
+	  
+	  $scope.ages = [0, 1, 2, 3]
+	  $scope.genders = [
+	                    {name: 'unspecified', value: 0}, 
+	                    {name: 'mixed', value: 1},
+	                    {name: 'female', value: 2},
+	                    {name: 'male', value: 3}
+	                    ]
   }])
   .controller('GetGames', ['$scope', 'Games', function($scope, Games) {
 	  $scope.games = Games.query();
